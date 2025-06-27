e4a1e50497c2d942cd3e0f49e3639d51631b93f6
beat2319 <beat2319@colorado.edu>
2025-06-27 15:06:38 -0600

    file upload route logic

--- javascript/backend-api/fileupload.js
+++ javascript/backend-api/fileupload.js
@@ -2,3 +2,18 @@ const express = require("express");
 const router = express.Router();
 const uploadImage = require("./multer-config.js");
 
+router.post("/", uploadImage, (req, res) => {
+    if(req.file){
+        return res.status(200).json({
+            sucess: true,
+            message: "Image uploaded!",
+        });
+    }
+
+    return res.status(400).json({
+        success: false,
+        error: "Image upload failed!",
+    });
+}); // post route to handle http request for file upload
+
+module.exports = router;
