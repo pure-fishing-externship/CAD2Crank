7a901a8467aaa4bb3df3bc1b60770c37be687598
beat2319 <beat2319@colorado.edu>
2025-06-27 15:30:51 -0600

    setting up ssr with vite - react

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    ssr: true,
+    rollupOptions: {
+      input: './server/index.js'
+    },
+  },
+});
